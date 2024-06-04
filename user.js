13b855bd992b6f058269501707cb0589c008a37b
Matthew <matthewrmuller@gmail.com>
2024-06-04 14:01:15 -0600

    commit

--- user.js
+++ user.js
@@ -1,22 +1,27 @@
 const postListEl = document.querySelector('.post-list')
-async function main() {
-  const id = localStorage.getItem("id");
-  const posts = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
-  );
-  const postsData = await posts.json();
-  console.log(postsData);
+const id = localStorage.getItem("id");
 
-  postListEl.innerHTML = postsData.map(
-    (post) => `<div class="post">
-    <div class="post__title">
-      Post Title
-    </div>
-    <p class="post__body">
-      Post Body
-    </p>
-  </div>`
-  ).join('')
+async function onSearchChange(event) {
+  const id = event.target.value
+  renderPosts(id)
 }
 
-main();
+async function renderPosts(id) {
+  
+  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+  const postsData = await posts.json();
+  postListEl.innerHTML = postsData.map((post) => postHTML(post)).join('')
+}
+
+function postHTML(post) {
+  return `<div class="post">
+  <div class="post__title">
+    ${post.title}
+  </div>
+  <p class="post__body">
+    ${post.body}
+  </p>
+</div>`
+}
+
+renderPosts(id);
